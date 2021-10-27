f37cbfd9c169dcdbe48e69cb22c138119d8fad63
sho naito <sho20100@gmail.com>
2021-10-27 17:21:22 +0900

    Change go:embed instead of ioutil.Read

--- slack/claim_modal.go
+++ slack/claim_modal.go
@@ -1,24 +1,18 @@
 package slack
 
 import (
+	_ "embed"
 	"fmt"
-	"io/ioutil"
 
 	"github.com/slack-go/slack"
 )
 
+//go:embed views/claim_modal.json
+var claimViewJson []byte
+
 func BuildClaimModalView() (*slack.Blocks, error) {
-
-	file := "slack/views/claim_modal.json"
-
-	bytes, err := ioutil.ReadFile(file)
-	if err != nil {
-		return nil, fmt.Errorf("Blocks JSON read fail  %w", err)
-	}
-
 	blocks := slack.Blocks{}
-
-	if err := blocks.UnmarshalJSON(bytes); err != nil {
+	if err := blocks.UnmarshalJSON(claimViewJson); err != nil {
 		return nil, fmt.Errorf("Blocks marshal error %w", err)
 	}
 
--- slack/home.go
+++ slack/home.go
@@ -1,24 +1,26 @@
 package slack
 
 import (
+	_ "embed"
 	"fmt"
-	"io/ioutil"
 
 	"github.com/slack-go/slack"
 )
 
+//go:embed views/home.json
+var homeViewJson []byte
+
 func BuildHomeView() (*slack.Blocks, error) {
 
-	file := "slack/views/home.json"
-
-	bytes, err := ioutil.ReadFile(file)
-	if err != nil {
-		return nil, fmt.Errorf("Blocks JSON read fail  %w", err)
-	}
+	// file := "slack/views/home.json"
+	// bytes, err := ioutil.ReadFile(file)
+	// if err != nil {
+	// 	return nil, fmt.Errorf("Blocks JSON read fail  %w", err)
+	// }
 
 	blocks := slack.Blocks{}
 
-	if err := blocks.UnmarshalJSON(bytes); err != nil {
+	if err := blocks.UnmarshalJSON(homeViewJson); err != nil {
 		return nil, fmt.Errorf("Blocks marshal error %w", err)
 	}
 
