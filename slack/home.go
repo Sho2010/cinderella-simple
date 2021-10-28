e172bad7fb2822e984dee6c961846979f5dce9f1
sho naito <sho20100@gmail.com>
2021-10-29 03:27:28 +0900

    Delete useless comment

--- slack/home.go
+++ slack/home.go
@@ -11,13 +11,6 @@ import (
 var homeViewJson []byte
 
 func BuildHomeView() (*slack.Blocks, error) {
-
-	// file := "slack/views/home.json"
-	// bytes, err := ioutil.ReadFile(file)
-	// if err != nil {
-	// 	return nil, fmt.Errorf("Blocks JSON read fail  %w", err)
-	// }
-
 	blocks := slack.Blocks{}
 
 	if err := blocks.UnmarshalJSON(homeViewJson); err != nil {
