91a8399f894c1be6aeed3896c0b99139ae0f6d9e
sho naito <sho20100@gmail.com>
2021-11-07 18:13:02 +0900

    tweak commnet position

--- slack/home_controller.go
+++ slack/home_controller.go
@@ -8,6 +8,19 @@ import (
 	"github.com/slack-go/slack"
 )
 
+// NOTE PublishView の引数に関して
+// - hash
+//   A string that represents view state to protect against possible race conditions.
+// - CallbackID
+//   表示したViewの識別子 以下のようにする予定
+//   cinderella_home_general
+//   cinderella_home_admin
+// - ExternalID
+//   こちらは種類ごとではなくて、表示されたModal1つ1つの識別コードのイメージ。Workspace内で一意にしておく必要があります。
+//
+//   おすすめは、ユーザー名+タイムスタンプ。
+//   これだと、よほどのことがない限り被らず安心です。
+
 var (
 	//go:embed views/home.json
 	homeViewJson []byte
@@ -24,19 +37,7 @@ type HomeController struct {
 }
 
 func (c *HomeController) Show(userID string) error {
-	// NOTE: hashに関して
-	// A string that represents view state to protect against possible race conditions.
 
-	// NOTE: CallbackID
-	// 表示したViewの識別子 以下のようにする予定
-	// cinderella_home_general
-	// cinderella_home_admin
-
-	// NOTE: ExternalID
-	// こちらは種類ごとではなくて、表示されたModal1つ1つの識別コードのイメージ。Workspace内で一意にしておく必要があります。
-	//
-	// おすすめは、ユーザー名+タイムスタンプ。
-	// これだと、よほどのことがない限り被らず安心です。
 
 	//TODO: 管理者固定になってる
 	blocks, err := c.buildHomeView(true)
