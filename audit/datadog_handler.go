9f711548904a756f8519cc0ac5efdc1f5109dbd1
sho naito <sho20100@gmail.com>
2021-11-12 12:31:52 +0900

    [WIP] datadog

---
+++ audit/datadog_handler.go
@@ -0,0 +1,60 @@
+package audit
+
+import (
+	"fmt"
+)
+
+type DatadogHandler struct {
+}
+
+var _ AuditEventHandler = &DatadogHandler{}
+
+func NewDatadogHandler() *DatadogHandler {
+	h := DatadogHandler{}
+	return &h
+}
+
+func (h *DatadogHandler) Start(ch <-chan AuditEvent) {
+	fmt.Println("Start DatadogHandler")
+
+	//TODO; imeplement me
+	for e := range ch {
+		fmt.Println(e)
+	}
+}
+
+// See: https://docs.datadoghq.com/ja/api/latest/events/
+//
+// Post an event returns "OK" response
+// package main
+//
+// import (
+// 	"context"
+// 	"encoding/json"
+// 	"fmt"
+// 	"os"
+//
+// 	datadog "github.com/DataDog/datadog-api-client-go/api/v1/datadog"
+// )
+//
+// func main() {
+// 	body := datadog.EventCreateRequest{
+// 		Title: "Example-Post_an_event_returns_OK_response",
+// 		Text:  "A text message.",
+// 		Tags: &[]string{
+// 			"test:ExamplePostaneventreturnsOKresponse",
+// 		},
+// 	}
+// 	ctx := datadog.NewDefaultContext(context.Background())
+// 	configuration := datadog.NewConfiguration()
+// 	apiClient := datadog.NewAPIClient(configuration)
+// 	resp, r, err := apiClient.EventsApi.CreateEvent(ctx, body)
+//
+// 	if err != nil {
+// 		fmt.Fprintf(os.Stderr, "Error when calling `EventsApi.CreateEvent`: %v\n", err)
+// 		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+// 	}
+//
+// 	responseContent, _ := json.MarshalIndent(resp, "", "  ")
+// 	fmt.Fprintf(os.Stdout, "Response from `EventsApi.CreateEvent`:\n%s\n", responseContent)
+// }
